1df6f8c1dd237902131906d37e893022a53901ae
Trevor Seitz <trevinla@gmail.com>
2022-04-29 13:12:43 -0400

    geting specifics

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 from wsgiref.util import request_uri
 import requests
-import apiKeys
+import apiKeys as apiKeys
 
 weather_key = apiKeys.API_KEY
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
@@ -12,6 +12,11 @@ response = requests.get(request_uri)
 
 if response.status_code == 200:
     data = response.json()
-    print(data)
+    print("Data: ", data)
+    weather = data['weather'][0]["description"]
+    print("Weather: ", weather)
+    temperatureC = round(data["main"]["temp"]-273.15, 1)
+    temperatureF = round(temperatureC*9/5 + 32, 1)
+    print("Temp: ", temperatureC, " C or ", temperatureF, " F")
 else:
     print("An error occurred")
